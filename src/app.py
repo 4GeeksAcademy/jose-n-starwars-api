d87c5c2cbd8908ad06293f6390f0837f90c6c4e4
jlnunezg10 <juniorng10@gmail.com>
2025-05-27 13:16:20 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, Blueprint, Response
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorites, Vehicules
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,185 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/people', methods = ['GET'])
+def get_people():
+
+    try:
+
+        people = People.query.all()
+
+        if not people:
+            return jsonify({'msg': "No se encontraron los personajes"}), 404
+        
+
+        people = list(map(lambda person: person.serialize(), people))
+        return jsonify(people),201
+
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_person(id):
+    try:
+
+        person = People.query.filter_by(id = id).first()
+
+        if not person:
+            return jsonify({'msg': f"No se encontraron el personaje {id}"}), 404
+        
+        return jsonify(person.serialize()),201
+
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+
+@app.route('/planets', methods = ['GET'])
+def get_planets():
+
+    try:
+
+        planets = Planet.query.all()
+
+        if not planets:
+            return jsonify({'msg': "No se encontraron los planetas"}), 404
+        
+
+        planets = list(map(lambda planet: planet.serialize(), planets))
+        return jsonify(planets), 201
+
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_planet(id):
+    try:
+
+        planet = Planet.query.filter_by(id = id).first()
+
+        if not planet:
+            return jsonify({'msg': "No se encontro el planeta"}), 404
+        
+        return jsonify(planet.serialize()), 201
+
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+
+
+@app.route('/users', methods = ['GET'])
+def get_users():
+    try:
+
+        users = User.query.all()
+
+        if not users:
+            return jsonify({'msg': "No existen usuarios registrados"})
+
+        users = list(map(lambda user: user.serialize(), users))
+
+        return jsonify(users), 201
+    
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+
+@app.route('/users/<int:user_id>/favorites', methods=['GET'])
+def get_favorites(user_id):
+    try:
+
+        favorites = Favorites.query.filter_by(user_id = user_id).all()
+
+        if not favorites:
+            return jsonify({'msg': f"El usuario {user_id} no tiene favoritos registrados"})
+        
+        favorites = list(map(lambda favorite: favorite.serialize(), favorites))
+
+        return jsonify(favorites), 201
+
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+    
+
+@app.route('/users/<int:user_id>/favorite/planet/<int:planet_id>', methods = ['POST'])
+def add_favorite_planet(user_id,planet_id):
+    try:
+
+        planet = Planet.query.filter_by(id = planet_id).first()
+        user = User.query.filter_by(id = user_id).first()
+
+        if not planet:
+            return jsonify({'msg': "El planeta elegido no existe"}), 404
+        if not user:
+            return jsonify({'msg': "El usuario elegido no existe"}), 404
+
+        new_favorite_planet = Favorites(user_id = user_id, planet_id = planet_id)
+        db.session.add(new_favorite_planet)
+        db.session.commit()
+
+        return jsonify(new_favorite_planet.serialize()), 201
+       
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+    
+@app.route('/users/<int:user_id>/favorite/people/<int:people_id>', methods = ['POST'])
+def add_favorite_people(user_id, people_id):
+    try:
+
+        person = People.query.filter_by(id = people_id).first()
+        user = User.query.filter_by(id = user_id).first()
+
+        if not person:
+            return jsonify({'msg': "El personaje elegido no existe"}), 404
+        if not user:
+            return jsonify({'msg': "El usuario elegido no existe"}), 404
+
+        new_favorite_people = Favorites(user_id = user_id, people_id = people_id)
+        db.session.add(new_favorite_people)
+        db.session.commit()
+
+        return jsonify(new_favorite_people.serialize()), 201
+       
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+
+@app.route('/users/<int:user_id>/favorite/people/<int:people_id>', methods = ['DELETE'])
+def delete_favorite_people(user_id,people_id):
+    try:
+
+        favorite = Favorites.query.filter_by(people_id = people_id, user_id = user_id).first()
+
+        if not favorite:
+            return jsonify({'msg': f"No existe el personaje {people_id} en favorito del usuario {user_id}"}), 404
+        
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({'msg': f"Personaje {people_id} eliminado satisfactoriamente de favoritos del usuario {user_id}"}) 
+
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+    
+@app.route('/users/<int:user_id>/favorite/planet/<int:planet_id>', methods = ['DELETE'])
+def delete_favorite_planet(user_id,planet_id):
+
+    try:
+
+        favorite = Favorites.query.filter_by(planet_id = planet_id, user_id = user_id).first()
+
+        if not favorite:
+            return jsonify({'msg': f"No existe el planeta {planet_id} en favorito del usuario {user_id}"}), 404
+        
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({'msg': f"Planeta {planet_id} eliminado satisfactoriamente de favoritos del usuario {user_id}"}) 
+
+    except Exception as error:
+        return jsonify({'msg': f"Ocurrio el siguiente error: {error}"}), 500
+    
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, Integer, Float
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 
 db = SQLAlchemy()
 
@@ -10,10 +10,86 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favorites: Mapped[list["Favorites"]] = relationship(back_populates="user", cascade="all, delete-orphan")
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id: Mapped[int]= mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    height: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+
+    favorites: Mapped[list["Favorites"]] = relationship(back_populates="people", cascade="all, delete-orphan")
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "birth_year":self.birth_year,
+            "height": self.height
+        }
+
+class Vehicules(db.Model):
+    id: Mapped[int]= mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    crew: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    passengers: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+
+    favorites: Mapped[list["Favorites"]] = relationship(back_populates="vehicules", cascade="all, delete-orphan")
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "crew":self.crew,
+            "passengers":self.passengers,
+        }
+
+class Planet(db.Model):
+    id: Mapped[int]= mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    population: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+
+    favorites: Mapped[list["Favorites"]] = relationship(back_populates="planet", cascade="all, delete-orphan")
+    
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "population":self.population,
+        }
+
+
+class Favorites(db.Model):
+    id: Mapped[int]= mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    people_id: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=True)
+    vehicules_id: Mapped[int] = mapped_column(ForeignKey("vehicules.id"), nullable=True)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=True)
+
+    user: Mapped["User"] = relationship(back_populates="favorites")
+    people: Mapped["People"] = relationship(back_populates="favorites")
+    vehicules: Mapped["Vehicules"] = relationship(back_populates="favorites")
+    planet: Mapped["Planet"] = relationship(back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "user_id": self.user.id,
+            "people_id": self.people_id,
+            "planet_id": self.planet_id,
+            "vehicules_id": self.vehicules_id,
+            
+            "people": self.people.serialize() if self.people else None,
+            "planet": self.planet.serialize() if self.planet else None,
+            "vehicules": self.vehicules.serialize() if self.vehicules else None,
+    } 
+        
+    
